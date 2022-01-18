b926e3a5aa58ee3657eb19c99bc5715f8119103e
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-18 19:26:56 +0100

    Update main.php

--- main.php
+++ main.php
@@ -1,35 +1,8 @@
 <?php
-require('config.php'); // this sets up $db
+require_once('config.php'); // this sets up $db
 
-try {
-    $dbh = new PDO("pgsql:host=" . $db['host'] . ";dbname=" . $db['dbname'] . ";port=" . $db['port'], 
-        $db['user'], $db['pass'], array(PDO::ATTR_PERSISTENT => true));	
-} catch(PDOException $e) {
-    echo "Error : " . $e->getMessage() . "<br/>";
-    die();
-}
+$rows = pg_query($conn, "SELECT * FROM influencer");
 
-$sql = "SELECT * FROM accounts";
-
-$stmt = $dbh->query($sql);
-
-if ($stmt===false){
-    echo "<pre>Error executing the query: $sql</pre>";
-    echo $stmt;
-    die();
-}
-
-
-    $accounts = array();
-    foreach ($stmt as $row){
-        $account = array(
-            "id" => $row['id'],
-            "name" => $row['username'],
-            "password" => $row['password'],
-            "email" => $row['email']
-        );
-        array_push($accounts, $account);
-    }
-    echo json_encode($accounts);
+echo json_encode($rows);
 
 ?>
