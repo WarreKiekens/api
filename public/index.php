a57e7e813c0e9fb03fbee4518523b94b7eb1bafd
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-18 20:39:32 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,8 +1,28 @@
 <?php
 include("../common/header.php");
 
-if (isset($_GET["details"]) && $_GET["details"]!="") {
-  $details = $_GET["details"];
-  echo $details;
+
+// Check if path start with /api
+if (explode("/",$_SERVER["PATH_INFO"])[1] != "api") {
+  echo "Wrong path<br>";
+  var_dump($_SERVER);
+  var_dump(explode("/",$_SERVER["PATH_INFO"]));
+  exit();
 }
+
+if ($_SERVER["REQUEST_METHOD"] === "GET") {
+  // api/influencer/{id}
+  if (isset($_GET["id"]) && $_GET["id"]!="") {
+    $id = $_GET["id"];
+  }
   
+  // api/influencer/{id}/posts
+  
+  // api/influencer/{id}/post/{id}
+}  
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {}
+
+if ($_SERVER["REQUEST_METHOD"] === "DELETE") {}
+
+if ($_SERVER["REQUEST_METHOD"] === "PUT") {}
