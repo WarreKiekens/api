960693a5b23150e21a475faacdab7fc1230b7c93
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-18 19:45:33 +0100

    Update get_query_data.php

--- common/get_query_data.php
+++ common/get_query_data.php
@@ -1,10 +1,9 @@
 <?php
-function get_query_data($query){
+  function get_query_data($query){
 
-  $rows = pg_query($conn, "SELECT * FROM influencer");
-  
-  return json_encode($rows);
-   
-}
+    $rows = pg_query($GLOBALS["conn"], $query);
+    $result = pg_fetch_all($rows);
 
+    return json_encode($result, JSON_PRETTY_PRINT);
+  }
 ?>
