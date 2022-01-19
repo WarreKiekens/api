a124f6c57d9b10dce769ac5c4c7dcab9242eff2f
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-19 13:51:17 +0100

    Update auth_update_token.php

--- functions/auth_update_token.php
+++ functions/auth_update_token.php
@@ -5,13 +5,11 @@
     
     // Genrate new token
     $token = bin2hex(random_bytes(64));
-    
-    $result = pg_update($GLOBALS["conn"], $type, array("token" => $token), array("id" => $id));
-    
+        
     // Get current epoch time on psql server
-    $epoch = get_query_data("select extract(epoch from now()) as epoch;");
+    $now = get_query_data("select now();");
     
-    $result2 = pg_update($GLOBALS["conn"], $type, array("token" => $token, "expireToken" => $epoch), array("id" => $id));
+    $result = pg_update($GLOBALS["conn"], $type, array("token" => $token, "expiretoken" => $now["now"]), array("id" => $id));
     
     if ($result) {
       return $token;
