8037ae49c90ed176f8dc57824884d98c391485bf
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-19 15:39:22 +0100

    Create auth_isvalid_token.php

---
+++ functions/auth_isvalid_token.php
@@ -0,0 +1,26 @@
+<?php
+  include_once("../config.php");
+
+  function auth_isvalid_token($token){
+        
+    // Check influencer
+    // 4f004dacb742f74acfd2919c92ec06c9912f78f91347ccd3281ea792368d8bdf3b8c77e7ce8782406928f654514f9c0704abda50b6b4884750b3e14dfa367185
+    $query = "SELECT count(*) as count FROM influencer WHERE token = '$token';";
+    $data = get_query_data($query);
+    
+    if ($data["count"] === "1") {
+      return array("valid" => true);
+    }
+    
+    // Check stad
+    $query = "SELECT count(*) FROM $type WHERE gebruikersnaam = '$username' and wachtwoord = '$password';";
+    $data = get_query_data($query);
+    
+    if ($data["count"] === "1") {
+      
+      return array("valid" => true, "id" => $data["id"]);
+    }
+    
+    return array("valid" => false, "id" => null);
+  };
+?>
