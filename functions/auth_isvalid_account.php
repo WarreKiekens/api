4941c9588a9669a4833ded678f9d3b069c5c5bba
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-19 11:17:15 +0100

    Update auth_isvalid_account.php

--- functions/auth_isvalid_account.php
+++ functions/auth_isvalid_account.php
@@ -4,12 +4,12 @@
   function auth_isvalid_account($username, $password, $type){
     include("../common/get_query_data.php");
     
-    $query = "SELECT COUNT(*) FROM $type WHERE gebruikersnaam = '$username' and password = '$password';";
+    $query = "SELECT count(*) as count FROM $type WHERE gebruikersnaam = '$username' and wachtwoord = '$password';";
     $data = get_query_data($query);
     
     $result = array();
     
-    if (count($data) == 1) {
+    if ($data["count"] === "1") {
       return true;
     } 
     return false;
