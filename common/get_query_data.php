53cf6743d99c1f6728d70fba42a2afc46f13ad16
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-20 15:33:46 +0100

    Update get_query_data.php

--- common/get_query_data.php
+++ common/get_query_data.php
@@ -9,4 +9,14 @@
     }
     return $result;
   }
+
+  function fetch_query_data($res){
+
+    $result = pg_fetch_all($res);
+
+    if (count($result) == 1) {
+      $result = $result[0];
+    }
+    return $result;
+  }
 ?>
