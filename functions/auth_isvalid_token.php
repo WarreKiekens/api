f5e10bfd018574da642f506aa43f35aa9da9e606
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-20 09:34:55 +0100

    Update auth_isvalid_token.php

--- functions/auth_isvalid_token.php
+++ functions/auth_isvalid_token.php
@@ -1,12 +1,27 @@
 <?php
   include_once("../config.php");
 
-  function auth_isvalid_token($token){
-        
-    // Check influencer
-    // Debug token
-    $token = '4f004dacb742f74acfd2919c92ec06c9912f78f91347ccd3281ea792368d8bdf3b8c77e7ce8782406928f654514f9c0704abda50b6b4884750b3e14dfa367185';
+  function auth_isvalid_token($token){    
     
+    // Debug token
+    //$token = '4f004dacb742f74acfd2919c92ec06c9912f78f91347ccd3281ea792368d8bdf3b8c77e7ce8782406928f654514f9c0704abda50b6b4884750b3e14dfa367185';
+    
+    // Validate Bearer
+    if (preg_match('/Bearer\s(\S+)/', $token, $matches)) {
+      $tokenString = $matches[1];
+      
+      if (ctype_alnum($tokenString)) {
+        // Valid Bearer
+      } else {
+        // Unvalid Bearer
+      }
+      
+    }
+    
+    
+    // Validate existing token
+    
+    // Check influencer
     $query = "SELECT count(*) as count FROM influencer WHERE token = '$token';";
     $data = get_query_data($query);
     
