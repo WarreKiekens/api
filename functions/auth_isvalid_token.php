97c3a16dab5ea34dd08582aa40f9b31580c793b4
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-20 09:12:19 +0100

    Update auth_isvalid_token.php

--- functions/auth_isvalid_token.php
+++ functions/auth_isvalid_token.php
@@ -5,7 +5,7 @@
         
     // Check influencer
     // Debug token
-    $token = '4f004dacb742f74acfd2919c92ec06c9912f78f91347ccd3281ea792368d8bdf3b8c77e7ce8782406928f654514f9c0704abda50b6b4884750b3e14dfa367185'
+    $token = '4f004dacb742f74acfd2919c92ec06c9912f78f91347ccd3281ea792368d8bdf3b8c77e7ce8782406928f654514f9c0704abda50b6b4884750b3e14dfa367185';
     
     $query = "SELECT count(*) as count FROM influencer WHERE token = '$token';";
     $data = get_query_data($query);
