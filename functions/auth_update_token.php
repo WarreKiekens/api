bcea9b4a73ab86d60d5880c7d1146c5b92165351
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-20 17:43:05 +0100

    Update auth_update_token.php

--- functions/auth_update_token.php
+++ functions/auth_update_token.php
@@ -7,9 +7,12 @@
     $token = bin2hex(random_bytes(64));
         
     // Get current epoch time on psql server
-    $now = get_query_data("select TO_CHAR(NOW(), 'DD-MM-YYYY HH:MI:SS') as now");
-    $expire = get_query_data("select TO_CHAR(NOW() + interval '1 day', 'DD-MM-YYYY HH:MI:SS') as expire");
-
+    $res = pg_query("select TO_CHAR(NOW(), 'DD-MM-YYYY HH:MI:SS') as now");
+    $now = fetch_query_data($res);
+    
+    $res = pq_query("select TO_CHAR(NOW() + interval '1 day', 'DD-MM-YYYY HH:MI:SS') as expire");
+    $expire = fetch_query_data($res);
+    
     $result = pg_update($GLOBALS["conn"], $type, array("token" => $token, "expiretoken" => $now["now"]), array("id" => $id));
     
     if ($result) {
