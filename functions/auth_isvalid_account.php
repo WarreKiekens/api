4801c1a4756ba2ca6ffd09926b0e82f105b5c988
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-20 19:24:27 +0100

    Update auth_isvalid_account.php

--- functions/auth_isvalid_account.php
+++ functions/auth_isvalid_account.php
@@ -3,17 +3,23 @@
 
   function auth_isvalid_account($username, $password, $type){
     
-    // Prevent Sql Injections
-    $username = pg_escape_string($username);
-    $password = pg_escape_string($password);
-    $type = pg_escape_string($type);
+    if (!in_array($type, ["stad", "influencer"])) {
+      return array("valid" => false, "code" => 400, "message" => "Type is expected to be of stad or influencer in body!", "data" => null);
+    }
     
-    $query = "SELECT count(*) as count FROM $type WHERE gebruikersnaam = '$username' and wachtwoord = '$password';";
-    $data = get_query_data($query);
+    $credentials = [$username, $password];
+    foreach ($credentials as $credential) {
+      if ($credential == "") {
+        return array("valid" => false, "code" => 400, "message" => "Username and/or password hasn't been provided in body!", "data" => null);
+      }
+    }
+    
+    $query = "SELECT count(*) as count FROM $type WHERE gebruikersnaam = $1 and wachtwoord = $2;";
+    $data = fetch_query_params($query, array($username, $password));
     
     if ($data["count"] === "1") {
-      $query = "SELECT id FROM $type WHERE gebruikersnaam = '$username' and wachtwoord = '$password';";
-      $data = get_query_data($query);
+      $query = "SELECT id FROM $type WHERE gebruikersnaam = $1 and wachtwoord = $2;";
+      $data = fetch_query_params($query, array($username, $password));
       
       return array("valid" => true, "id" => $data["id"]);
     } 
