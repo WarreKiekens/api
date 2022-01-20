ee456cb9a74e91e28fb59d36bc6a161a64f49a0b
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-20 09:17:51 +0100

    Update get_details_influencer.php

--- functions/get_details_influencer.php
+++ functions/get_details_influencer.php
@@ -2,7 +2,6 @@
   include_once("../config.php");
 
   function get_details_influencer($id){
-    include("../common/get_query_data.php");
     
     $query = "SELECT voornaam,familienaam,geslacht FROM Influencer where id = $id";
     $data = get_query_data($query);
