495d8187561ead6db2185d4c7df272ed20d6b29c
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-21 08:55:06 +0100

    Update get_query_data.php

--- common/get_query_data.php
+++ common/get_query_data.php
@@ -1,18 +1,5 @@
 <?php
 
-////
-  function get_query_data($query){
-
-    $rows = pg_query($GLOBALS["conn"], $query);
-    $result = pg_fetch_all($rows);
-
-    if (count($result) == 1) {
-      $result = $result[0];
-    }
-    return $result;
-  }
-////
-
   function fetch_query_data($res){
     
     $result = pg_fetch_all($res);
