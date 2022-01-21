b0babab36c6f5a7ea1be0b5228d1423e827fa531
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-21 15:04:39 +0100

    Update header.php

--- common/header.php
+++ common/header.php
@@ -1,5 +1,5 @@
 <?php
-//header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json; charset=UTF-8');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header("Access-Control-Allow-Headers: X-Requested-With");
