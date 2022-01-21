2cb1c6f2f1b716c0297f594b5692c828bee4ba85
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-22 00:41:23 +0100

    Create get_details_cities.php

---
+++ functions/get_details_cities.php
@@ -0,0 +1,17 @@
+<?php
+  include_once("../config.php");
+
+  function get_details_cities(){
+    
+    // Authorization
+    // Everyone can access this resource
+            
+    $res = pg_query("SELECT id,naam,postcode stad;");
+    $data = fetch_query_data($res);
+    
+    if ($data == null) {
+      return array("valid" => false, "code" => "500", "message" => "PSQL statement couldn't be executed!", "error" => "InternalError");
+    } 
+    return array("valid" => true, "data" => $data);
+  };
+?>
