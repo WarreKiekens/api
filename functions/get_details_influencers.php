69218d0138e8f17d5eac34fcb6ccc65b71ef9004
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-21 10:09:32 +0100

    Create get_details_influencers.php

---
+++ functions/get_details_influencers.php
@@ -0,0 +1,19 @@
+<?php
+  include_once("../config.php");
+
+  function get_details_influencers(){
+    
+    // Authorization
+    if (!in_array($GLOBALS["type"], array("stad"))) {
+      return array("valid" => false, "code" => 401, "message" => "Unauthorized to access this resource", "error" => "ForbiddenContent");
+    }
+            
+    $res = pg_query("SELECT voornaam,familienaam,geslacht FROM Influencer;");
+    $data = fetch_query_data($res);
+    
+    if ($data == null) {
+      return array("valid" => false, "code" => "500", "message" => "PSQL statement couldn't be executed!", "error" => "InternalError");
+    } 
+    return array("valid" => true, "data" => $data);
+  };
+?>
