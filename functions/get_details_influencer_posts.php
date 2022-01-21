e93ede355b4a3609376252fe0e05c29f5d5285bc
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-21 11:33:16 +0100

    Update get_details_influencer_posts.php

--- functions/get_details_influencer_posts.php
+++ functions/get_details_influencer_posts.php
@@ -1,15 +1,19 @@
 <?php
   include_once("../config.php");
 
-  function get_details_influencer_posts(){
+  function get_details_influencer_posts($influencerId){
+    
+    if (!is_numeric($influencerId)){
+      return array("valid" => false, "code" => 422, "message" => "The type of given Entity isn't supported!!", "error" => "UnprocessableEntity");
+    }
     
     // Authorization
     if (!in_array($GLOBALS["account_type"], array("stad"))) {
       return array("valid" => false, "code" => 401, "message" => "Unauthorized to access this resource", "error" => "ForbiddenContent");
     }
-            
-    $res = pg_query("SELECT * FROM Post;");
-    $data = fetch_query_data($res);
+                
+    $query = "SELECT * FROM post WHERE influencerid = $1";
+    $data = fetch_query_params($query, array($influencerId));
     
     if ($data == null) {
       return array("valid" => false, "code" => "500", "message" => "PSQL statement couldn't be executed!", "error" => "InternalError");
