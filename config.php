0ccc6a0e3db3794e7d6b656e44d22d75892b8ac4
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-21 17:41:43 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,12 @@
 <?php
   $host = "192.168.56.115";
+  //$host = "192.168.56.101";
+
   $dbname = "application";
   $user = "application";
   $password = "warre";
+  //$password = "application";
+  
 
   $conn = pg_connect("host=$host dbname=$dbname user=$user password=$password");
 
