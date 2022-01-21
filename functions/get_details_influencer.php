fd344bdfeeba6fa9b4b6d546babecd59b69aff2d
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-21 09:27:41 +0100

    Update get_details_influencer.php

--- functions/get_details_influencer.php
+++ functions/get_details_influencer.php
@@ -6,6 +6,9 @@
     $query = "SELECT voornaam,familienaam,geslacht FROM Influencer where id = $1";
     $data = fetch_query_params($query, array($id));
     
-    return $data;
+    if ($data == null) {
+      return array("valid" => false, "code" => 200, "message" => "Index out of reach!", "error" => "IndexOverflow");
+    } 
+    return array("valid" => true, "data" => $data);
   };
 ?>
