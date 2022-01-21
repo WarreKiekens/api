a367a6770ee629c79a70f8b581d76eceaacde27d
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-21 09:17:23 +0100

    Update auth_isvalid_account.php

--- functions/auth_isvalid_account.php
+++ functions/auth_isvalid_account.php
@@ -4,13 +4,13 @@
   function auth_isvalid_account($username, $password, $type){
     
     if (!in_array($type, ["stad", "influencer"])) {
-      return array("valid" => false, "code" => 400, "message" => "Type is expected to be of stad or influencer in body!", "data" => null);
+      return array("valid" => false, "code" => 400, "message" => "Type is expected to be of stad or influencer in body!", "error" => "AuthTypeInvalid");
     }
     
     $credentials = [$username, $password];
     foreach ($credentials as $credential) {
       if ($credential == "") {
-        return array("valid" => false, "code" => 400, "message" => "Username and/or password hasn't been provided in body!", "data" => null);
+        return array("valid" => false, "code" => 400, "message" => "Username and/or password hasn't been provided in body!", "error" => "AuthCredsInvalid");
       }
     }
     
@@ -23,6 +23,6 @@
       
       return array("valid" => true, "id" => $data["id"]);
     } 
-    return array("valid" => false, "code" => 401, "message" => "Account doesn't exist or credentials/username is wrong!", "data" => null);
+    return array("valid" => false, "code" => 401, "message" => "Account doesn't exist or credentials/username is wrong!", "error" => "AuthCredsWrong");
   };
 ?>
