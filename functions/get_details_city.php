bb87e12378acb20a9744fa6d1fe9a01999e32a33
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-22 01:03:01 +0100

    Create get_details_city.php

---
+++ functions/get_details_city.php
@@ -0,0 +1,25 @@
+<?php
+  include_once("../config.php");
+
+  function get_details_city($id){
+        
+    if (!is_numeric($id)){
+      return array("valid" => false, "code" => 422, "message" => "The type of given Entity isn't supported!!", "error" => "UnprocessableEntity");
+    }
+    
+    // Authorization
+    if (!in_array($GLOBALS["account_type"], array("stad"))){
+      //if ($GLOBALS["account_id"] != $id) {
+      return array("valid" => false, "code" => 401, "message" => "Unauthorized to access this resource", "error" => "ForbiddenContent");
+      //}
+    }
+        
+    $query = "SELECT id,naam,postcode FROM stad where id = $1";
+    $data = fetch_query_params($query, array($id));
+    
+    if ($data == null) {
+      return array("valid" => false, "code" => 200, "message" => "Index out of reach!", "error" => "IndexOverflow");
+    } 
+    return array("valid" => true, "data" => $data);
+  };
+?>
