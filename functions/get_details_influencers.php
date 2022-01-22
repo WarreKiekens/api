d1da8c751fb31a1f1e81d1de979858c34a86f1e0
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-22 15:16:25 +0100

    Update get_details_influencers.php

--- functions/get_details_influencers.php
+++ functions/get_details_influencers.php
@@ -5,7 +5,7 @@
     
     // Authorization
     if (!in_array($GLOBALS["account_type"], array("stad"))) {
-      return array("valid" => false, "code" => 401, "message" => "Unauthorized to access this resource", "error" => "ForbiddenContent");
+      return array("valid" => false, "code" => 403, "message" => "Unauthorized to access this resource", "error" => "ForbiddenContent");
     }
             
     $res = pg_query("SELECT id,voornaam,familienaam,geslacht,gebruikersnaam,profielfoto,adres,postcode,stad,geboortedatum,telefoonnummer,emailadres,gebruikersnaamInstagram,gebruikersnaamFacebook,gebruikersnaamTiktok,infoovervolgers,badge,aantalpunten,(select STRING_AGG (naam, ';') AS column FROM categorie where categorie.id in (select categorieid from influencercategorie where influencerid = influencer.id)) as categories FROM Influencer ORDER BY ID;");
