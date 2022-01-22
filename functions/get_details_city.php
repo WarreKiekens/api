c40122b3083fe98947195e3f6c176fcc0fea0bb9
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-22 14:30:32 +0100

    Update get_details_city.php

--- functions/get_details_city.php
+++ functions/get_details_city.php
@@ -10,7 +10,7 @@
     // Authorization
     if (!in_array($GLOBALS["account_type"], array("stad"))){
       //if ($GLOBALS["account_id"] != $id) {
-      return array("valid" => false, "code" => 401, "message" => "Unauthorized to access this resource", "error" => "ForbiddenContent");
+      // return array("valid" => false, "code" => 401, "message" => "Unauthorized to access this resource", "error" => "ForbiddenContent");
       //}
     }
         
