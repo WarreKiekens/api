0beb1db3a2916e101718aa8362ce31f8f73ad1b4
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-22 14:55:10 +0100

    Create get_details_influencer_post.php

---
+++ functions/get_details_influencer_post.php
@@ -0,0 +1,26 @@
+<?php
+  include_once("../config.php");
+
+  function get_details_influencer_posts($influencerId, $postId){
+    
+    if (!is_numeric($influencerId) or !is_numeric($postId)){
+      return array("valid" => false, "code" => 422, "message" => "The type of given Entity isn't supported!!", "error" => "UnprocessableEntity");
+    }
+    
+    // Authorization
+    if (!in_array($GLOBALS["account_type"], array("stad"))) {
+      if ($GLOBALS["account_id"] != $influencerId) {
+        return array("valid" => false, "code" => 401, "message" => "Unauthorized to access this resource", "error" => "ForbiddenContent");
+      }
+    }
+                
+    $query = "SELECT * FROM post WHERE influencerid = $1 and id = $2";
+    $data = fetch_query_params($query, array($influencerId, $postId));
+    
+    if ($data == null) {
+      return array("valid" => false, "code" => 200, "message" => "Index out of reach!", "error" => "IndexOverflow");
+    } 
+ 
+    return array("valid" => true, "data" => $data);
+  };
+?>
