214e24cf10af75f053c0ea8d8cd8f61ef42405dc
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-24 14:14:55 +0100

    Update auth_create_account.php

--- functions/auth_create_account.php
+++ functions/auth_create_account.php
@@ -1,7 +1,13 @@
 <?php
   include_once("../config.php");
 
-  function auth_create_account($type, $fields){
+  function auth_create_account($fields){
+    
+    $type = $fields["type"];
+    
+    if (!in_array($type, ["stad", "influencer"])) {
+      return array("valid" => false, "code" => 400, "message" => "Type is expected to be of stad or influencer in body!", "error" => "AuthTypeInvalid");
+    }
     
     // Check if account already exists
     $query = "SELECT count(*) as count FROM $type WHERE gebruikersnaam = $1;";
