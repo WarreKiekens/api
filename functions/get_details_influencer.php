27f242635fbdce7ac75cb542305e18ebe4c92a0c
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-24 09:58:48 +0100

    Update get_details_influencer.php

--- functions/get_details_influencer.php
+++ functions/get_details_influencer.php
@@ -15,6 +15,9 @@
     }
         
     $query = "SELECT voornaam,familienaam,geslacht FROM Influencer where id = $1";
+
+    $query = "SELECT id,voornaam,familienaam,geslacht,gebruikersnaam,profielfoto,adres,postcode,stad,geboortedatum,telefoonnummer,emailadres,gebruikersnaamInstagram,gebruikersnaamFacebook,gebruikersnaamTiktok,infoovervolgers,AantalVolgersInstagram,AantalVolgersFacebook,AantalVolgersTiktok,badge,aantalpunten,(select STRING_AGG (naam, ';') AS column FROM categorie where categorie.id in (select categorieid from influencercategorie where influencerid = $1)) as categories FROM Influencer WHERE id = $1 ORDER BY ID;";
+    
     $data = fetch_query_params($query, array($id));
     
     if ($data == null) {
