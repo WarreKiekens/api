53f01ebd3124a9fdf00713ccb8beab9342d378d8
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-25 10:55:02 +0100

    Update get_query_data.php

--- common/get_query_data.php
+++ common/get_query_data.php
@@ -4,13 +4,26 @@
     
     $result = pg_fetch_all($res);
 
-    //if (count($result) == 1) {
-    //  $result = $result[0];
-    //}
+    if (count($result) == 1) {
+      $result = $result[0];
+    }
     return $result;
   }
 
   function fetch_query_params($query, $array){
+    $res = pg_query_params($GLOBALS["conn"], $query, $array);
+    return fetch_query_data($res);
+  }
+
+  // RAW
+  function fetch_query_data_raw($res){
+    
+    $result = pg_fetch_all($res);
+    
+    return $result;
+  }
+
+  function fetch_query_params_raw($query, $array){
     $res = pg_query_params($GLOBALS["conn"], $query, $array);
     return fetch_query_data($res);
   }
