5d1abd157b0fb7946f68b21e3092053aa9b76aaa
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-25 11:48:09 +0100

    Update get_query_data.php

--- common/get_query_data.php
+++ common/get_query_data.php
@@ -16,17 +16,4 @@
     return fetch_query_data($res);
   }
 
-  // RAW
-  function fetch_query_data_raw($res){
-    
-    $result = pg_fetch_all($res);
-    
-    return $result;
-  }
-
-  function fetch_query_params_raw($query, $array){
-    $res = pg_query_params($GLOBALS["conn"], $query, $array);
-    return fetch_query_data_raw($res);
-  }
-
 ?>
