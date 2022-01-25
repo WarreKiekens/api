87218eb615de49e9d5f050b1213b0123bb772ea7
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-25 14:14:25 +0100

    Update get_details_city_influencers.php

--- functions/get_details_city_influencers.php
+++ functions/get_details_city_influencers.php
@@ -12,7 +12,7 @@
       return array("valid" => false, "code" => 403, "message" => "Unauthorized to access this resource", "error" => "ForbiddenContent");
     }
                 
-    $query = "SELECT * FROM stad WHERE id = $1";
+    $query = "select id,voornaam,familienaam,geslacht,gebruikersnaam,profielfoto,adres,postcode,stad,geboortedatum,telefoonnummer,emailadres,gebruikersnaamInstagram,gebruikersnaamFacebook,gebruikersnaamTiktok,infoovervolgers,AantalVolgersInstagram,AantalVolgersFacebook,AantalVolgersTiktok,badge,aantalpunten,(select STRING_AGG (naam, ';') AS column FROM categorie where categorie.id in (select categorieid from influencercategorie where influencerid = influencer.id)) as categories from influencer where id in (select influencerid from influencerstad where stadid = $1);";
     $data = fetch_query_params($query, array($cityId));
     
     if ($data == null) {
