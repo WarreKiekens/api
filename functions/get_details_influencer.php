9eb27755a00c2a52b41eeb8c07cc6e5ed6505ad4
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-25 11:03:19 +0100

    Update get_details_influencer.php

--- functions/get_details_influencer.php
+++ functions/get_details_influencer.php
@@ -18,7 +18,7 @@
 
     $query = "SELECT id,voornaam,familienaam,geslacht,gebruikersnaam,profielfoto,adres,postcode,stad,geboortedatum,telefoonnummer,emailadres,gebruikersnaamInstagram,gebruikersnaamFacebook,gebruikersnaamTiktok,infoovervolgers,AantalVolgersInstagram,AantalVolgersFacebook,AantalVolgersTiktok,badge,aantalpunten,(select STRING_AGG (naam, ';') AS column FROM categorie where categorie.id in (select categorieid from influencercategorie where influencerid = $1)) as categories FROM Influencer WHERE id = $1 ORDER BY ID;";
     
-    $data = fetch_query_params($query, array($id));
+    $data = fetch_query_params_raw($query, array($id));
     
     if ($data == null) {
       return array("valid" => false, "code" => 200, "message" => "Index out of reach!", "error" => "IndexOverflow");
