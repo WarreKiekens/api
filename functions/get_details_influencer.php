a23fcafd0d0fcc3e98b7bd4310a77632157cf4bd
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-25 13:31:37 +0100

    Update get_details_influencer.php

--- functions/get_details_influencer.php
+++ functions/get_details_influencer.php
@@ -13,8 +13,6 @@
         return array("valid" => false, "code" => 403, "message" => "Unauthorized to access this resource", "error" => "ForbiddenContent");
       }
     }
-        
-    $query = "SELECT voornaam,familienaam,geslacht FROM Influencer where id = $1";
 
     $query = "SELECT id,voornaam,familienaam,geslacht,gebruikersnaam,profielfoto,adres,postcode,stad,geboortedatum,telefoonnummer,emailadres,gebruikersnaamInstagram,gebruikersnaamFacebook,gebruikersnaamTiktok,infoovervolgers,AantalVolgersInstagram,AantalVolgersFacebook,AantalVolgersTiktok,badge,aantalpunten,(select STRING_AGG (naam, ';') AS column FROM categorie where categorie.id in (select categorieid from influencercategorie where influencerid = $1)) as categories FROM Influencer WHERE id = $1 ORDER BY ID;";
     
@@ -22,7 +20,15 @@
     
     if ($data == null) {
       return array("valid" => false, "code" => 200, "message" => "Index out of reach!", "error" => "IndexOverflow");
-    } 
+    }
+    
+    // Convert categories into proper array
+    $index = 0;
+    foreach ($data as $influencer){ 
+      $data[$index]["categories"] = explode(";", $influencer["categories"]);
+      $index++; 
+    }
+    
     return array("valid" => true, "data" => $data);
   };
 ?>
