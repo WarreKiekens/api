30ef5cf35e78475c21c2c54019c44f32a0464571
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-26 14:38:55 +0100

    Update put_update_account.php

--- functions/put_update_account.php
+++ functions/put_update_account.php
@@ -43,15 +43,7 @@
         "isaangevuld" => $fields["iscompleted"],
         "isactief" => $fields["isactive"],
         "aantalpunten" => $fields["totalpoints"],       
-      );
-      
-      // unset all null values
-      
-      foreach($values as $key=>$value){
-        if(is_null($value) || $value == '')
-            unset($values[$key]);
-      }
-      
+      );      
     
     } elseif ($GLOBALS["account_type"] == "stad") {
       
@@ -65,7 +57,12 @@
       ); 
     } 
 
-    
+
+    // unset all null values
+    foreach($values as $key=>$value){
+      if(is_null($value) || $value == '')
+          unset($values[$key]);
+    }
     
     $result = pg_update($GLOBALS["conn"], $GLOBALS["account_type"], $values, array("id" => $GLOBALS["account_id"]));
     
