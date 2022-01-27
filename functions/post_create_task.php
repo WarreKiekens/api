eb84ea90205861fafbdc6b4f5468e15dc097cb99
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-27 13:37:37 +0100

    Create post_create_task.php

---
+++ functions/post_create_task.php
@@ -0,0 +1,63 @@
+<?php
+  include_once("../config.php");
+
+  function auth_create_task($fields){
+    
+    // ALLOW CITY TO CREATE TASK
+    
+    $type = $fields["type"];
+    
+    if (!in_array($type, ["stad", "influencer"])) {
+      return array("valid" => false, "code" => 400, "message" => "Type is expected to be of stad or influencer in body!", "error" => "AuthTypeInvalid");
+    }
+    
+    // Check if account already exists
+    $query = "SELECT count(*) as count FROM $type WHERE gebruikersnaam = $1;";
+    $data = fetch_query_params($query, array($fields["username"]))[0];
+    
+    if ($data["count"] >= 1) {
+      return array("valid" => false, "code" => "409", "message" => "Username already exists!", "error" => "AccountExists");
+    }
+    
+    
+    if ($type == "influencer") {
+      
+      // TODO: validate input
+      $values = array(
+        "gebruikersnaam" => $fields["username"],
+        "wachtwoord" => $fields["password"],
+        "emailadres" => $fields["email"],
+      );
+      
+      $result = pg_insert($GLOBALS["conn"], $type, $values);
+      
+    } elseif ($type == "stad") {
+      
+      
+      // TODO: validate input
+      $values = array(
+        "gebruikersnaam" => $fields["username"],
+        "wachtwoord" => $fields["password"],
+        "naam" => $fields["name"],
+        "postcode" => $fields["postcode"],
+        "emailadres" => $fields["email"],
+      ); 
+      
+      
+      $result = pg_insert($GLOBALS["conn"], $type, $values);
+      
+      
+    
+    } else {
+      return array("valid" => false, "code" => "500", "message" => "Internal referral type not allowed!", "error" => "InternalError");
+    
+    }
+    
+    if ($result) {
+      return array("valid" => true);
+    }
+
+    return array("valid" => false, "code" => "500", "message" => "PSQL statement couldn't be executed!", "error" => "InternalError");  
+    
+  }
+?>
