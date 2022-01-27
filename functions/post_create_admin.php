d867eca155f36d6d1dcc3bb0f0b75efc1792d0f5
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-27 14:02:42 +0100

    Create post_create_admin.php

---
+++ functions/post_create_admin.php
@@ -0,0 +1,41 @@
+<?php
+  include_once("../config.php");
+
+  function auth_create_admin($fields){
+
+    
+    // Authorization: Check if account is super user
+    $query = "SELECT isSuper FROM admin WHERE id = $1;";
+    $data = fetch_query_params($query, array($GLOBALS["account_id"]))[0];
+    if ($data["isSuper"] == "f"){
+        return array("valid" => false, "code" => 403, "message" => "Unauthorized to create this resource", "error" => "ForbiddenContent");
+    }
+    
+    // Check if account already exists
+    $query = "SELECT count(*) as count FROM admin WHERE gebruikersnaam = $1;";
+    $data = fetch_query_params($query, array($fields["username"]))[0];
+    
+    if ($data["count"] >= 1) {
+      return array("valid" => false, "code" => "409", "message" => "Username already exists!", "error" => "AccountExists");
+    }
+    
+      
+    // TODO: validate input
+    $values = array(
+      "gebruikersnaam" => $fields["username"],
+      "voornaam" => $fields["firstname"],
+      "familienaam" => $fields["lastname"],
+      "wachtwoord" => $fields["password"],
+      "emailadres" => $fields["email"],
+    ); 
+
+    $result = pg_insert($GLOBALS["conn"], "admin", $values);
+      
+    if ($result) {
+      return array("valid" => true);
+    }
+
+    return array("valid" => false, "code" => "500", "message" => "PSQL statement couldn't be executed!", "error" => "InternalError");  
+    
+  }
+?>
