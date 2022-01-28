eca97fe13bf697c456a67a363f84f5428bfb47fb
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-28 13:49:57 +0100

    Update auth_isvalid_account.php

--- functions/auth_isvalid_account.php
+++ functions/auth_isvalid_account.php
@@ -3,9 +3,9 @@
 
   function auth_isvalid_account($username, $password, $type){
     
-    if (!in_array($type, ["stad", "influencer","admin"])) {
-      return array("valid" => false, "code" => 400, "message" => "Type is expected to be of stad or influencer in body!", "error" => "AuthTypeInvalid");
-    }
+    //if (!in_array($type, ["stad", "influencer","admin"])) {
+    //  return array("valid" => false, "code" => 400, "message" => "Type is expected to be of stad or influencer in body!", "error" => "AuthTypeInvalid");
+    //}
     
     $credentials = [$username, $password];
     foreach ($credentials as $credential) {
@@ -14,25 +14,32 @@
       }
     }
     
-    $query = "SELECT count(*) as count FROM $type WHERE gebruikersnaam = $1 and wachtwoord = $2;";
-    $data = fetch_query_params($query, array($username, $password))[0];
+    // Search for type
+    $types = ["stad", "influencer", "admin"];
+    foreach ($type as $types) {   
     
-    if ($data["count"] === "1") {
-      
-      // Check if account has been deactivated
-      $query = "SELECT id,isactief FROM $type WHERE gebruikersnaam = $1 and wachtwoord = $2;";
+      $query = "SELECT count(*) as count FROM $type WHERE gebruikersnaam = $1 and wachtwoord = $2;";
       $data = fetch_query_params($query, array($username, $password))[0];
-      
-      if ($data["isactief"] == "f") {
-        if ($type == "influencer") {
-          return array("valid" => false, "code" => 403, "message" => "Account is disabled! If this is an error please contact the administrator to resolve this issue.", "error" => "AuthAccountDisabled");
+
+      if ($data["count"] === "1") {
+
+        // Check if account has been deactivated
+        $query = "SELECT id,isactief FROM $type WHERE gebruikersnaam = $1 and wachtwoord = $2;";
+        $data = fetch_query_params($query, array($username, $password))[0];
+
+        if ($data["isactief"] == "f") {
+          if ($type == "influencer") {
+            return array("valid" => false, "code" => 403, "message" => "Account is disabled! If this is an error please contact the administrator to resolve this issue.", "error" => "AuthAccountDisabled");
+          }
+          return array("valid" => false, "code" => 403, "message" => "Account is not verified yet! The administrator is still processing your request.", "error" => "AuthAccountNotVerified");
+
         }
-        return array("valid" => false, "code" => 403, "message" => "Account is not verified yet! The administrator is still processing your request.", "error" => "AuthAccountNotVerified");
-        
-      }
-      
-      return array("valid" => true, "id" => $data["id"]);
-    } 
+
+        return array("valid" => true, "id" => $data["id"]);
+      } 
+    
+    }
+    
     return array("valid" => false, "code" => 401, "message" => "Account doesn't exist or credentials/username is wrong!", "error" => "AuthCredsWrong");
   };
 ?>
