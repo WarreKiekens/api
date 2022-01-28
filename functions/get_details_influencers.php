36ed60bd45cc4a4806b2efb81859bd598cea25a7
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-28 13:36:04 +0100

    Update get_details_influencers.php

--- functions/get_details_influencers.php
+++ functions/get_details_influencers.php
@@ -7,9 +7,18 @@
     if (!in_array($GLOBALS["account_type"], array("stad"))) {
       return array("valid" => false, "code" => 403, "message" => "Unauthorized to access this resource", "error" => "ForbiddenContent");
     }
-            
-    $res = pg_query("SELECT id,voornaam,familienaam,geslacht,gebruikersnaam,profielfoto,adres,postcode,stad,geboortedatum,telefoonnummer,emailadres,gebruikersnaamInstagram,gebruikersnaamFacebook,gebruikersnaamTiktok,infoovervolgers,AantalVolgersInstagram,AantalVolgersFacebook,AantalVolgersTiktok,badge,aantalpunten,(select STRING_AGG (naam, ';') AS column FROM categorie where categorie.id in (select categorieid from influencercategorie where influencerid = influencer.id)) as categories FROM Influencer ORDER BY ID;");
-    $data = fetch_query_data($res);
+    
+    if (isset($_GET["where"]) and isset($_GET["like"])) {
+      
+      $data = filtering_influencers();
+      
+      if ($data == null) {
+        return array("valid" => true, "code" => 200, "message" => "Influencers successfully requested!");
+      }
+    } else {
+      $res = pg_query("SELECT id,voornaam,familienaam,geslacht,gebruikersnaam,profielfoto,adres,postcode,stad,geboortedatum,telefoonnummer,emailadres,gebruikersnaamInstagram,gebruikersnaamFacebook,gebruikersnaamTiktok,infoovervolgers,AantalVolgersInstagram,AantalVolgersFacebook,AantalVolgersTiktok,badge,aantalpunten,(select STRING_AGG (naam, ';') AS column FROM categorie where categorie.id in (select categorieid from influencercategorie where influencerid = influencer.id)) as categories FROM Influencer ORDER BY ID;");
+      $data = fetch_query_data($res);
+    }
     
     if ($data == null) {
       return array("valid" => false, "code" => "500", "message" => "PSQL statement couldn't be executed!", "error" => "InternalError");
