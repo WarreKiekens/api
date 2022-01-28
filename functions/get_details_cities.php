b6c0ffd389599de13153ee9ff8d97add10fc14c1
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-28 10:26:55 +0100

    Update get_details_cities.php

--- functions/get_details_cities.php
+++ functions/get_details_cities.php
@@ -5,9 +5,14 @@
     
     // Authorization
     // Everyone can access this resource
-            
-    $res = pg_query("SELECT id,naam,postcode FROM stad ORDER BY id");
-    $data = fetch_query_data($res);
+    
+    if (isset($_GET["where"]) and isset($_GET["like"])) {
+        $query = pg_query("SELECT id,naam,postcode,isactief FROM stad WHERE $1 like '%$2%' ORDER BY id");
+        $data = fetch_query_params($query, array($_GET["where"], $_GET["like"]));
+    } else {        
+      $res = pg_query("SELECT id,naam,postcode,isactief FROM stad ORDER BY id");
+      $data = fetch_query_data($res);
+    }
     
     if ($data == null) {
       return array("valid" => false, "code" => 500, "message" => "PSQL statement couldn't be executed!", "error" => "InternalError");
