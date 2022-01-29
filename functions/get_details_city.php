19a4b320ba885a4f55dcca193d09d60b24abfb74
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-29 09:34:28 +0100

    Update get_details_city.php

--- functions/get_details_city.php
+++ functions/get_details_city.php
@@ -14,7 +14,7 @@
       //}
     }
         
-    $query = "SELECT id,naam,postcode FROM stad where id = $1";
+    $query = "SELECT id,naam,postcode,emailadres FROM stad where id = $1";
     $data = fetch_query_params($query, array($id));
     
     if ($data == null) {
