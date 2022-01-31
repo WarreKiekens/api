93319809aa271b3b1907be87187040f7653a9c8a
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-01-31 13:03:28 +0100

    Update filtering.php

--- common/filtering.php
+++ common/filtering.php
@@ -14,7 +14,7 @@ function filtering_cities() {
   // check if value is bool
   if (in_array($_GET["like"], array("t","f"))) {
 
-    $query = "SELECT id,naam,postcode,isactief,isnew,emailadres FROM stad WHERE {$_GET['where']} = $1 ORDER BY id";
+    $query = "SELECT id,naam,postcode,isactief,isnew,emailadres,(select count(influencerid) from InfluencerStad where stadid = stad.id) as influencercount FROM stad WHERE {$_GET['where']} = $1 ORDER BY id";
 
     if ($_GET["like"] == "t") {
       $data = fetch_query_params($query, array('true'));
@@ -25,7 +25,7 @@ function filtering_cities() {
   } else {
 
     //TODO: check if where value in cols 
-    $query = "SELECT id,naam,postcode,isactief,isnew,emailadres FROM stad WHERE position($1 in {$_GET['where']}) > 0 ORDER BY id";
+    $query = "SELECT id,naam,postcode,isactief,isnew,emailadres,(select count(influencerid) from InfluencerStad where stadid = stad.id) as influencercount FROM stad WHERE position($1 in {$_GET['where']}) > 0 ORDER BY id";
     $data = fetch_query_params($query, array($_GET["like"]));
 
   }
