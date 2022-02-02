fd384aadd9c0df2c2f8deadb12619889f3499e2b
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-02-02 09:33:55 +0100

    Create get_details_task.php

---
+++ functions/get_details_task.php
@@ -0,0 +1,25 @@
+<?php
+  include_once("../config.php");
+
+  function get_details_task($id){
+        
+    if (!is_numeric($id)){
+      return array("valid" => false, "code" => 422, "message" => "The type of given Entity isn't supported!", "error" => "UnprocessableEntity");
+    }
+    
+    // Authorization
+    if (!in_array($GLOBALS["account_type"], array("influencer"))){
+      //if ($GLOBALS["account_id"] != $id) {
+      // return array("valid" => false, "code" => 401, "message" => "Unauthorized to access this resource", "error" => "ForbiddenContent");
+      //}
+    }
+        
+    //$query = "SELECT id,gebruikersnaam,naam,postcode,isactief,isnew,emailadres,(select count(influencerid) from InfluencerStad where stadid = stad.id) as influencercount  FROM stad where id = $1";
+    //$data = fetch_query_params($query, array($id));
+    
+    if ($data == null) {
+      return array("valid" => false, "code" => 200, "message" => "Index out of reach!", "error" => "IndexOverflow");
+    } 
+    return array("valid" => true, "data" => $data);
+  };
+?>
