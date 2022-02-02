7cdd12cf350285381f1ed2f7bffa37b9dd5d2491
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-02-02 14:47:24 +0100

    Update get_details_task.php

--- functions/get_details_task.php
+++ functions/get_details_task.php
@@ -8,12 +8,16 @@
     }
     
     // Authorization
-    if (!in_array($GLOBALS["account_type"], array("influencer"))){
-      //if ($GLOBALS["account_id"] != $id) {
-      // return array("valid" => false, "code" => 401, "message" => "Unauthorized to access this resource", "error" => "ForbiddenContent");
-      //}
+    if (!in_array($GLOBALS["account_type"], array("stad"))){
+      return array("valid" => false, "code" => 401, "message" => "Unauthorized to access this resource", "error" => "ForbiddenContent");
     }
         
+    // check if given id is actually an task of city id account
+    $query = "SELECT count(*) FROM opdracht where stadid = $1";
+    $data = fetch_query_params($query, array($GLOBALS["account_id"]));
+    
+    var_dump($data);
+    
     //$query = "SELECT id,gebruikersnaam,naam,postcode,isactief,isnew,emailadres,(select count(influencerid) from InfluencerStad where stadid = stad.id) as influencercount  FROM stad where id = $1";
     //$data = fetch_query_params($query, array($id));
     
