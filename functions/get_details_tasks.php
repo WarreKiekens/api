c3c65081e9f9a7320994ed329c1cbaf86f403da6
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-02-02 09:51:32 +0100

    Update get_details_tasks.php

--- functions/get_details_tasks.php
+++ functions/get_details_tasks.php
@@ -4,19 +4,16 @@
   function get_details_tasks(){
     
     // Authorization
-    if (!in_array($GLOBALS["account_type"], array("stad"))) {
+    if (!in_array($GLOBALS["account_type"], array("stad","influencer"))) {
       return array("valid" => false, "code" => 403, "message" => "Unauthorized to access this resource", "error" => "ForbiddenContent");
     }
-    
-    if (isset($_GET["where"]) and isset($_GET["like"])) {
-      
-      //$data = filtering_tasks();
-      
-      if ($data == null) {
-        return array("valid" => true, "code" => 200, "message" => "Tasks successfully requested!");
-      }
-    } else {
+
+   
+    if ($GLOBALS["account_type"] == "stad") {
       $query = "SELECT * FROM opdracht where stadid = $1 ORDER BY ID;";
+      $data = fetch_query_params($query, array($GLOBALS["account_id"]));
+    } else {
+      $query = "SELECT * FROM opdracht where categorie in (...) ORDER BY ID;";
       $data = fetch_query_params($query, array($GLOBALS["account_id"]));
     }
     
