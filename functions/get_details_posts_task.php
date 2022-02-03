d8ba0be3c50f09366237b0cfb95dbfa83828db56
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-02-03 12:58:25 +0100

    Update get_details_posts_task.php

--- functions/get_details_posts_task.php
+++ functions/get_details_posts_task.php
@@ -1,8 +1,11 @@
 <?php
   include_once("../config.php");
 
-  function get_details_posts_task(){
+  function get_details_posts_task($id){
     
+    if (!is_numeric($id)){
+      return array("valid" => false, "code" => 422, "message" => "The type of given Entity isn't supported!", "error" => "UnprocessableEntity");
+    }
     
     // TODO: request all posts of a task, done by a city (are only allowed to query their own tasks)
     
@@ -12,8 +15,19 @@
       return array("valid" => false, "code" => 403, "message" => "Unauthorized to access this resource", "error" => "ForbiddenContent");
     }
     
-    $res = pg_query("SELECT id,voornaam,familienaam,geslacht,gebruikersnaam,profielfoto,adres,postcode,stad,geboortedatum,telefoonnummer,emailadres,gebruikersnaamInstagram,gebruikersnaamFacebook,gebruikersnaamTiktok,infoovervolgers,AantalVolgersInstagram,AantalVolgersFacebook,AantalVolgersTiktok,badge,aantalpunten,(select STRING_AGG (naam, ';') AS column FROM categorie where categorie.id in (select categorieid from influencercategorie where influencerid = influencer.id)) as categories FROM Influencer ORDER BY ID;");
-    $data = fetch_query_data($res);
+    
+    // check if given id is actually an task of city id account
+    $query = "SELECT id FROM opdracht where stadid = $1";
+    $data = fetch_query_params($query, array($GLOBALS["account_id"]));
+    
+    if (!in_array($id, $data)) {
+      return array("valid" => false, "code" => 403, "message" => "Unauthorized to access this resource", "error" => "ForbiddenContent");
+      
+    }
+    
+    
+    $query = "SELECT * FROM post where stadid = $1 and opdrachtid = $2 ORDER BY ID;";
+    $data = fetch_query_params($query, array($GLOBALS["account_id"], $id));
 
     if ($data == null) {
       return array("valid" => false, "code" => "500", "message" => "PSQL statement couldn't be executed!", "error" => "InternalError");
