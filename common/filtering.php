7309aedfc48a94fe3e28734291b79e4f58c30f74
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-02-04 11:23:12 +0100

    Update filtering.php

--- common/filtering.php
+++ common/filtering.php
@@ -43,11 +43,15 @@ function filtering_influencers() {
   if (in_array($_GET["where"], array("volgers","Volgers"))) {
     switch ($_GET["like"]) {
       case "<500":
-        echo "zzzz";
+        $query = "SELECT id,voornaam,familienaam,geslacht,gebruikersnaam,profielfoto,adres,postcode,stad,geboortedatum,telefoonnummer,emailadres,gebruikersnaamInstagram,gebruikersnaamFacebook,gebruikersnaamTiktok,infoovervolgers,AantalVolgersInstagram,AantalVolgersFacebook,AantalVolgersTiktok,badge,aantalpunten,(select STRING_AGG (naam, ';') AS column FROM categorie where categorie.id in (select categorieid from influencercategorie where influencerid = influencer.id)) as categories, (select EXTRACT(YEAR FROM age(now(), geboortedatum))) as leeftijd FROM Influencer WHERE (aantalvolgersinstagram < 500 and aantalvolgersfacebook < 500 and aantalvolgerstiktok < 500) {$_GET['where']} = $1 and id in (select influencerid from influencerstad where stadid = $2) ORDER BY ID;";
+        $data = fetch_query_params($query, array($_GET["like"], $GLOBALS["account_id"]));
+        
         break;
       default:
         break;
     }
+    
+    
   } if (in_array($_GET["where"], array("geslacht","Geslacht"))) {
     $query = "SELECT id,voornaam,familienaam,geslacht,gebruikersnaam,profielfoto,adres,postcode,stad,geboortedatum,telefoonnummer,emailadres,gebruikersnaamInstagram,gebruikersnaamFacebook,gebruikersnaamTiktok,infoovervolgers,AantalVolgersInstagram,AantalVolgersFacebook,AantalVolgersTiktok,badge,aantalpunten,(select STRING_AGG (naam, ';') AS column FROM categorie where categorie.id in (select categorieid from influencercategorie where influencerid = influencer.id)) as categories, (select EXTRACT(YEAR FROM age(now(), geboortedatum))) as leeftijd FROM Influencer WHERE {$_GET['where']} = $1 and id in (select influencerid from influencerstad where stadid = $2) ORDER BY ID;";
     $data = fetch_query_params($query, array($_GET["like"], $GLOBALS["account_id"]));
