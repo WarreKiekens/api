b1e01295b85507382116af19071c2b21cfd8b522
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-02-04 09:37:52 +0100

    Update get_details_influencer.php

--- functions/get_details_influencer.php
+++ functions/get_details_influencer.php
@@ -14,6 +14,14 @@
       }
     }
 
+    
+    $query = "influencerid from influencerStad where stadid = $1";
+    $influencers = fetch_query_params($query, array($id));
+    
+    if (!in_array($id, $influencers)) {
+        return array("valid" => false, "code" => 403, "message" => "Unauthorized to access this resource", "error" => "ForbiddenContent");
+    }
+
     $query = "SELECT id,voornaam,familienaam,geslacht,gebruikersnaam,profielfoto,adres,postcode,stad,geboortedatum,telefoonnummer,emailadres,gebruikersnaamInstagram,gebruikersnaamFacebook,gebruikersnaamTiktok,infoovervolgers,AantalVolgersInstagram,AantalVolgersFacebook,AantalVolgersTiktok,badge,aantalpunten,(select STRING_AGG (naam, ';') AS column FROM categorie where categorie.id in (select categorieid from influencercategorie where influencerid = $1)) as categories FROM Influencer WHERE id = $1 ORDER BY ID;";
     
     $data = fetch_query_params($query, array($id));
