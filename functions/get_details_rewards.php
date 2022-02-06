0ec8f80e098067a35734ec8fd54892e30c81faf6
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-02-06 17:11:13 +0100

    Update get_details_rewards.php

--- functions/get_details_rewards.php
+++ functions/get_details_rewards.php
@@ -1,3 +1,27 @@
 <?php
+  include_once("../config.php");
 
+  function get_details_rewards($id){
+    
+    if (!is_numeric($id)){
+      return array("valid" => false, "code" => 422, "message" => "The type of given Entity isn't supported!", "error" => "UnprocessableEntity");
+    }
+    
+     // Authorization
+    if ( !in_array($GLOBALS["account_type"], array("influencer")) or ($GLOBALS["account_id"] != $id) ){
+      return array("valid" => false, "code" => 403, "message" => "Unauthorized to access this resource", "error" => "ForbiddenContent");
+    }
+
+    // all accepted rewards
+    $query = "select * from reward where id in (select rewardid from influencerreward where influencerid = $1);";
+    $data1 = fetch_query_params($query, array($id));
+
+    // all other rewards
+    $query = "select * from reward where id not in (select id from reward where id in (select rewardid from influencerreward where influencerid = $1));";
+    $data2 = fetch_query_params($query, array($id));
+    
+    $allRewards = array_merge($data1,$data2);
+    
+    return array("valid" => true, "data" => $data);
+  };
 ?>
