6a161b33b1749a6d83288b03b5d9aac513cf8a76
WarreKiekens <57798995+WarreKiekens@users.noreply.github.com>
2022-02-09 18:03:23 +0100

    Update put_update_reward.php

--- functions/put_update_reward.php
+++ functions/put_update_reward.php
@@ -16,16 +16,14 @@
       // Add
       
       $values = array(
-        "gebruikersnaam" => $fields["username"],
-        "wachtwoord" => $fields["password"],
-        "emailadres" => $fields["email"],
+        "rewardid" => $fields["rewardid"],
       );
       
-      $result = pg_insert($GLOBALS["conn"], $type, $values);
+      $result = pg_insert($GLOBALS["conn"], "influencerreward", $values);
       
     } elseif ($fields["type"] == "unclaim") {
-       // Remove
-
+      // Remove
+      
       $query = "SELECT * from influencerreward where rewardid = $1";      
       $influencerids = fetch_query_params($query, array($GLOBALS['account_id']));
 
@@ -35,7 +33,7 @@
       }
 
       if (!in_array($fields["rewardid"], $origin)){
-          return array("valid" => false, "code" => 403, "message" => "Unauthorized to access this resource", "error" => "ForbiddenContent");
+          return array("valid" => false, "code" => 403, "message" => "This resource doesn't exist.", "error" => "ForbiddenContent");
       }
 
       // check if task already ended
@@ -46,16 +44,11 @@
           return array("valid" => false, "code" => 422, "message" => "Task already ended, unable to update.", "error" => "UnprocessableEntity");
       }
 
+      
       $values = array(
-        "titel" => $fields["title"],
-        "omschrijving" => $fields["description"],
-        "aantalpuntenwaard" => $fields["totalpointsworth"],
-        "isuitgevoerd" => false,
-        "datumopgegeven" => $now["now"],
-        "foto" => $fields["picture"],
-        "winnaarid" => $fields["winnerid"],
-
+        "rewardid" => $fields["rewardid"],
       );
+      
 
       // unset all null values
       foreach($values as $key=>$value){
@@ -63,18 +56,8 @@
             unset($values[$key]);
       }
 
-      // If winner is set, add datumuitgevoerd,isuitgevoerd=>true,winnaarid
-      if ($fields["winnerid"]) {
-          $res = pg_query("select TO_CHAR(NOW(), 'YYYY-MM-DD HH:MI:SS') as now");
-          $now = fetch_query_data($res)[0];
+      $result = pg_delete($GLOBALS["conn"], 'infuencerreward', $values);
 
-          $values["datumuitgevoerd"] = $now["now"];
-          $values["isuitgevoerd"] = true;
-          $values["winnaarid"] = $fields["winnerid"];
-
-      }
-
-      $result = pg_update($GLOBALS["conn"], "opdracht", $values, array("id" => $fields["taskid"]));
     
     } else {
       return array("valid" => false, "code" => 422, "message" => "The type is not of claim or unclaim!", "error" => "UnprocessableEntity");
